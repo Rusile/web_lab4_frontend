8496994e468a66453166d2d4001ca7ed77dd3374
Ildar <ildarelmeev@yandex.com>
2023-01-19 18:15:31 +0300

    fix graph

--- src/components/MainBody/components/CoordinatesForm/CoordinatesFields.jsx
+++ src/components/MainBody/components/CoordinatesForm/CoordinatesFields.jsx
@@ -38,7 +38,6 @@ const CoordinatesFields = (props) => {
                 <FormControlLabel value={2} control={<Radio/>} label="2"/>
                 <FormControlLabel value={3} control={<Radio/>} label="3"/>
                 <FormControlLabel value={4} control={<Radio/>} label="4"/>
-                <FormControlLabel value={5} control={<Radio/>} label="5"/>
             </RadioGroup>
             <TextField className="coordinates-form-fields-y-field"
                        variant="outlined"
@@ -56,11 +55,6 @@ const CoordinatesFields = (props) => {
                 <FormControlLabel value={2} control={<Radio/>} label="2"/>
                 <FormControlLabel value={3} control={<Radio/>} label="3"/>
                 <FormControlLabel value={4} control={<Radio/>} label="4"/>
-                <FormControlLabel value={5} control={<Radio/>} label="5"/>
-                <FormControlLabel value={6} control={<Radio/>} label="6"/>
-                <FormControlLabel value={7} control={<Radio/>} label="7"/>
-                <FormControlLabel value={8} control={<Radio/>} label="8"/>
-                <FormControlLabel value={9} control={<Radio/>} label="9"/>
             </RadioGroup>
         </div>
     )
